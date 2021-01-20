2d992b2b63cb9c27fa2a4b8766728458145cd156
Abdul Aziz Ansari <abdulaziz.ansari@digitas.com>
2021-01-20 18:45:06 +0530

    1. providers and consumers

---
+++ lib/model/task_data.dart
@@ -0,0 +1,26 @@
+import 'dart:collection';
+
+import 'package:flutter/foundation.dart';
+import 'package:state_management_examples/model/task.dart';
+
+class TaskData extends ChangeNotifier {
+  final List<Task> _tasks = [];
+
+  void addTask(String task) {
+    _tasks.add(Task(name: task));
+    notifyTaskDataListeners();
+  }
+
+  /// don't use this to add data into list
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
+  int get length {
+    return _tasks.length;
+  }
+
+  void notifyTaskDataListeners() {
+    notifyListeners();
+  }
+}
